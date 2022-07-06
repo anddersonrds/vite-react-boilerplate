bf0c4481bb8c32ad4f5726047614172d8dee725c
Anderson Rodrigues <anddersonrds@gmail.com>
2022-07-05 23:23:19 -0300

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import logo from './assets/svg/logo.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
-
-export default App
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        I hope you enjoy using our boilerplate, we set it up thinking about
+        optimizing your studies :)
+      </p>
+      <p>
+        Edit <code>App.tsx</code> and save to test HMR updates.
+      </p>
+      <p>
+        Developed by{' '}
+        <a
+          href="https://github.com/anddersonrds"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @anddersonrds
+        </a>
+        .
+      </p>
+    </header>
+  </div>
+)
